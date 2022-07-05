a2cc63c1d2953d16b2fb95b775feaec19d1947af
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2022-07-05 20:23:28 +0200

    migrated to Java 9

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module dk.eSoftware.commandLineParser {
+    exports dk.eSoftware.commandLineParser;
+    exports dk.eSoftware.commandLineParser.generalized;
+    exports dk.eSoftware.commandLineParser.generalized.documentation;
+    exports dk.eSoftware.commandLineParser.generalized.annotations;
+}
